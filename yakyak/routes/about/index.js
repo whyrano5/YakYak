3d658a532e1822191d104c57097c3ec78e706bf3
BaeMinji <balswl0229@naver.com>
2021-06-10 20:16:23 +0900

    YakYak

--- yakyak/routes/about/index.js
+++ yakyak/routes/about/index.js
@@ -30,28 +30,20 @@ router.get('/', function(req, res, next){
     else{
         var sql = 'SELECT * FROM pill WHERE 품목명 LIKE \'\%'+data+'\%\' OR  분류명 LIKE \'\%'+data+'\%\' OR 업소명 LIKE \'\%'+data+'\%\'';
     }
-
-    var best_sql = "SELECT DISTINCT COUNT(*) AS 리뷰개수, 전문일반구분, 분류명, 품목명, 업소명, 성상, 의약품제형, 큰제품이미지 FROM pill, review WHERE 품목명=review_yakname GROUP BY 분류명 ORDER BY 리뷰개수 DESC";
-      
+ 
     conn.query(sql, function (err, rows, fields) {
         if(err) console.log('query is not excuted. select fail...\n' + err);
         else {
-            conn.query(best_sql, function (err, rows2, fields) {
-                if(err) console.log('query is not excuted. select fail...\n' + err);
-                else {
-                    console.log("!22");
-                    if(req && req.session && req.session.count) {
-                        // 세션이 존재하는 경우
-                        res.render('about.ejs',{user:req.session.uid, rows: rows, rows2:rows2, page:page, length:rows.length-1, page_num:10, pass:true,data1:data});
-                        console.log(rows);
-                
-                    } else { 
-                        // 세션이 존재하지 않는 경우
-                        var id='';
-                        res.render('about.ejs', {user:id, rows: rows, rows2:rows2, page:page, length:rows.length-1, page_num:10, pass:true,data1:data});
-                    }
-                }
-            });
+            if(req && req.session && req.session.count) {
+                // 세션이 존재하는 경우
+                res.render('about.ejs',{user:req.session.uid, rows: rows, page:page, length:rows.length-1, page_num:10, pass:true,data1:data});
+                console.log(rows);
+        
+            } else { 
+                // 세션이 존재하지 않는 경우
+                var id='';
+                res.render('about.ejs', {user:id, rows: rows, page:page, length:rows.length-1, page_num:10, pass:true,data1:data});
+            }
         }
     });
 })
